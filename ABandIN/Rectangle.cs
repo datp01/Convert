7e01bc9bc007d79eaef64a56f213b905eeef32e9
datp01 <97713597+datp01@users.noreply.github.com>
2022-08-28 23:53:34 +0700

    Bai CSharp OOP

---
+++ ABandIN/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABandIN
+{
+    internal class Rectangle
+    {
+        int Width;
+        int Height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+            for (int i = 1; i< this.Height -1;i++)
+            {
+                DrawLine(this.Width, ' ', '*');
+            }
+            DrawLine(this.Width, '*', '*');
+        }
+        private void DrawLine(int Width,char end,char mid)
+        {
+            Console.Write(end);
+            for ( int i = 1;i<Width -1;i++)
+            {
+                Console.Write(mid);
+            }
+            Console.Write(end);
+        }
+    }
+}
---
+++ ABandIN/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABandIN
+{
+    internal class Startup: IDrawable
+    {
+        static void Main()
+        {
+
+            var Radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(Radius);
+
+            var Width = int.Parse(Console.ReadLine());
+            var Height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(Width, Height);
+
+            circle.Draw();
+            rect.Draw();
+
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
